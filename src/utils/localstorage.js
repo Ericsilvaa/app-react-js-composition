c349b66400849acdf8105d4fc359606f3b8c152a
Eric <eriicsilva28@gmail.com.br>
2024-08-02 19:22:50 -0300

    feat: criação de funções utilitarias

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -1,7 +1,15 @@
 // localStorage
-const getLocalStorage = () =>
-  JSON.parse(localStorage.getItem('db_client')) || []
-const setLocalStorage = (dbClient) =>
-  localStorage.setItem('db_client', JSON.stringify(dbClient))
+const getLocalStorage = () => {
+  const data = JSON.parse(localStorage.getItem('db_clients')) || []
+  console.log('🚀 ~ getLocalStorage ~ data:', data)
+  return data
+}
+
+const setLocalStorage = (dbClient) => {
+  const data =
+    localStorage.setItem('db_clients', JSON.stringify(dbClient)) || []
+
+  return data
+}
 
 export { getLocalStorage, setLocalStorage }
---
+++ src/utils/valid-fields.js
@@ -0,0 +1,27 @@
+const validFields = (formData) => {
+  const { nome, email, celular, cidade } = formData
+  console.log('🚀 ~ validFields ~ formData:', formData)
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  console.log('🚀 ~ validFields ~ PASSED')
+  if (!nome || !email || !celular || !cidade) {
+    alert('Todos os campos são obrigatórios.')
+    return false
+  }
+
+  if (!emailRegex.test(email)) {
+    alert('Por favor, insira um e-mail válido.')
+    return false
+  }
+  console.log('🚀 ~ validFields ~ PASSED.test(email)')
+
+  return true
+}
+
+const clearFields = () => ({
+  nome: '',
+  email: '',
+  celular: '',
+  cidade: ''
+})
+
+export { clearFields, validFields }
