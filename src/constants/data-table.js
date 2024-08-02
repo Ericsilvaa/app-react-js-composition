a5bb140c581968fb8c13f1bb308de82a5454e2fb
Eric <eriicsilva28@gmail.com.br>
2024-08-02 19:23:06 -0300

    feat: criação de dados constantes

---
+++ src/constants/data-table.js
@@ -0,0 +1,16 @@
+const columnsTable = ['Nome', 'E-mail', 'Celular', 'Cidade', 'Ação']
+
+const actionsTable = [
+  {
+    id: 'editar',
+    label: 'Editar',
+    color: 'blue'
+  },
+  {
+    id: 'excluir',
+    label: 'Excluir',
+    color: 'red'
+  }
+]
+
+export { actionsTable, columnsTable }
