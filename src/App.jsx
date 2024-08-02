384c03c05e5fae0725a2ebf1c9f3aaaeafe46df9
Eric <eriicsilva28@gmail.com.br>
2024-08-02 17:14:51 -0300

    feat: incrementando app e main.js

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import HomePage from './pages/Home/HomePage'
+
+function App() {
+  return <HomePage />
+}
+
+export default App
