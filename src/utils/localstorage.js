93d808e5c7f773497f51662fb6c308f505abd24f
Eric <eriicsilva28@gmail.com.br>
2024-08-02 17:21:37 -0300

    feat: criação de utils para acessar localstorage

---
+++ src/utils/localstorage.js
@@ -0,0 +1,7 @@
+// localStorage
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem('db_client')) || []
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem('db_client', JSON.stringify(dbClient))
+
+export { getLocalStorage, setLocalStorage }
