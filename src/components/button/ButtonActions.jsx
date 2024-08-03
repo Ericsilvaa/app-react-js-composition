0ae5f0270564bf70523fa817de6cade3b9c32715
Eric <eriicsilva28@gmail.com.br>
2024-08-03 01:45:39 -0300

    refactor: refatorando componente form

--- src/components/button/ButtonActions.jsx
+++ src/components/button/ButtonActions.jsx
@@ -1,45 +1,22 @@
 import PropTypes from 'prop-types'
-import { useClientContext } from '../../hooks/useClientContext'
-import { useModalContext } from '../../hooks/useModalContext'
-import CustomButton from './button'
+import { TableAction } from '../table/TableAction'
 import './button.css'
 
-const InfoActions = ({ renderActions, clientId }) => {
-  const { deleteItem, getById } = useClientContext()
-  const { openModal } = useModalContext()
-
-  const handleClick = (btn) => {
-    const handleDeleteItem = () => {
-      deleteItem(clientId)
-    }
-
-    const handleOpenModal = () => {
-      getById(btn.clientId)
-      openModal()
-    }
-
-    return btn.id === 'editar' ? handleOpenModal : handleDeleteItem
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }
-
+const InfoActions = ({ actions, onClick, clientId }) => {
   return (
     <div className='button-actions-container'>
-      {renderActions.map((btn) => (
-        <CustomButton
-          key={btn.id}
-          id={btn.id}
-          classNames={`button ${btn.color}`}
-          onClick={handleClick({ ...btn, clientId })}
-        >
-          {btn.label}
-        </CustomButton>
-      ))}
+      {actions.map((btn) => {
+        const dataValue = { ...btn, onClick, clientId }
+
+        return <TableAction key={btn.id} data={dataValue} />
+      })}
     </div>
   )
 }
 
 InfoActions.propTypes = {
-  renderActions: PropTypes.array,
+  actions: PropTypes.array,
+  onClick: PropTypes.func,
   clientId: PropTypes.number
 }
 
